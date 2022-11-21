ec5c0ff3cd04fbbede8cf86eba8f283e5d84c431
Dmitry Sheshukov <yooyklon@gmail.com>
2022-11-21 17:13:01 +0700

    two commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function ShopItemFunc(props) {
+  return (
+    <div className="main-content">
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div className="description">{props.item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {props.item.currency}
+          {props.item.price}
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
